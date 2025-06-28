49fac139db3fbacae66d3e434c8b4c77fc02404f
VAISHNAVI GUPTA <guptavaishnavi76002@gmail.com>
2025-06-28 02:56:03 +0000

    initia; commit

---
+++ practice1.js
@@ -0,0 +1,12 @@
+const  accountId = 123456789
+let accountEmail = guptavaishanvi@gmail.com
+var accountPassword = "1234"
+accountCity = "delhi"
+let accountantName = "vaishnavi"
+let accountState;
+
+// accountId = 2 // not allowed
+
+console.table([accountId],
+    [accountEmail],[accountPassword],[accountCity],[accountName],[accountState]
+)
